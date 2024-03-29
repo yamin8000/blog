11a768b1376921840b3c66a68fef6ab385a84ef3
Yamin Siahmargooei <yamin8000@yahoo.com>
2024-03-30 00:38:43 +0330

    tailwind added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
-<h1>Welcome to my blog!</h1>
+<div class="p-4">
+    <h1 class="text-3xl font-bold text-center">
+        Welcome to my blog!
+    </h1>
+    <p>Work in progress!</p>
+</div>
+
+<style lang="postcss">
+    :root {
+        color-scheme: dark light;
+    }
+</style>
