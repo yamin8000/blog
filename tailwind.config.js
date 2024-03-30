12eaa7737fac314fea0ac4e306139babf6e4f5e6
Yamin Siahmargooei <yamin8000@yahoo.com>
2024-03-30 21:11:36 +0330

    added nav, still wip!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./**/*.html"],
     theme: {
         extend: {},
+        colors: {
+            ...colors
+        }
     },
     plugins: [],
 }
